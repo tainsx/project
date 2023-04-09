2b756805313b507a9649ddda0c8407d9a0a48ab3
tainsx <omergundogdu034@gmail.com>
2023-04-10 00:41:13 +0300

    project directory now tracked by git

---
+++ daire.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main() {
+
+// dairenin alanýný bulan program
+
+float r,circumference,area;
+
+const float PI=3.1415;
+
+printf("Dairenin yari capini girin:");
+scanf("%f",&r);
+circumference= 2*PI*r;
+area=PI*r*r;
+printf("circumference = %f \n",circumference);
+printf("area = %f \n",area);
+
+
+
+
+
+
+return 0;
+
+
+}
+
